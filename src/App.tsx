6dc30b3735f07ffb6201f37cd29cde70bfe8221d
Georgi <figoto660@gmail.com>
2022-10-28 22:52:53 -0400

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Nav from "./components/Nav";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
