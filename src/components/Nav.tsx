b22feee61b0ce367096ead0204e0134cb8b7c70c
Georgi <figoto660@gmail.com>
2022-10-29 00:06:42 -0400

    add log in and out

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,32 @@
 import { Link } from "react-router-dom";
+import { signOut } from "firebase/auth";
 import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Nav = () => {
+  const [user] = useAuthState(auth);
+  const logOut = async () => {
+    await signOut(auth);
+  };
+
   return (
     <div>
       <Link to="/"> Home</Link>
       <Link to="/login"> Login</Link>
       <div>
-        <p>{auth.currentUser?.displayName}</p>
-        <img src={auth.currentUser?.photoURL || ""} alt="missing pic" width="100" height="100"></img>
+        {user && (
+          <div>
+            {" "}
+            <p>{auth.currentUser?.displayName}</p>
+            <img
+              src={auth.currentUser?.photoURL || ""}
+              alt="missing pic"
+              width="100"
+              height="100"
+            ></img>
+            <button onClick={logOut}>Log Out</button>
+          </div>
+        )}
       </div>
     </div>
   );
