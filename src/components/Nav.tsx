5dbc65199b978cdf36f2c6694c97df0b3b80914a
Georgi <figoto660@gmail.com>
2022-10-28 23:55:19 -0400

    add google login

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
 
 const Nav = () => {
   return (
-    <div><Link to="/"> Home</Link>
-        <Link to="/login"> Login</Link>
+    <div>
+      <Link to="/"> Home</Link>
+      <Link to="/login"> Login</Link>
+      <div>
+        <p>{auth.currentUser?.displayName}</p>
+        <img src={auth.currentUser?.photoURL || ""} alt="missing pic" width="100" height="100"></img>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,21 @@
+import { provider, auth } from "../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    const navigate = useNavigate();
+  const singInGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    console.log(result);
+    navigate("/");
+  };
 
-export default Login
+  return (
+    <div>
+      <p>Sing in with Google!</p>
+      <button onClick={singInGoogle}>Google</button>
+    </div>
+  );
+};
+
+export default Login;
