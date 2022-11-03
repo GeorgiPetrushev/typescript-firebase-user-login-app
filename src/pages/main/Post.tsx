b3fe32508f5ed79b3bd5b0c65b7320e8dd56a009
Georgi <figoto660@gmail.com>
2022-11-02 21:15:26 -0700

    .

--- src/pages/main/Post.tsx
+++ src/pages/main/Post.tsx
@@ -1,9 +1,8 @@
 import { addDoc, collection, query, where, getDocs } from "firebase/firestore";
-import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../../config/firebase";
 import { Post as IPost } from "./Home";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 interface Props {
   post: IPost;
@@ -16,20 +15,21 @@ const Post = (props: Props) => {
   const [likeAmount, setLikeAmount] = useState<number | null>(null);
 
   const likesRef = collection(db, "likes");
-  const likesDoc = query(likesRef, where("post", "==", post.id));
+  const likesDoc = query(likesRef, where("postId", "==", post.id));
 
   const getLikes = async () => {
     const data = await getDocs(likesDoc);
-    console.log(data.docs);
+    setLikeAmount(data.docs.length);
   };
 
   const addLike = async () => {
     await addDoc(likesRef, { userId: user?.uid, postId: post.id });
+    getLikes();
   };
 
   useEffect(() => {
-    getLikes();
-  }, []);
+    
+  },[]);
 
   return (
     <div key={post.id}>
