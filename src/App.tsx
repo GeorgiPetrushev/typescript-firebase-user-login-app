9942fb78482d28e96b00ad1fe94322c9fbf27a4e
Georgi <figoto660@gmail.com>
2022-11-04 22:14:14 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <h1>Test</h1>
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
